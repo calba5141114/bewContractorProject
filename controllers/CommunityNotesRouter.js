749ef80cb65932ca0e86c9646157e711f27c1e47
Carlos Alba <calba5141114@Carloss-MacBook-Pro.local>
2018-10-12 13:16:44 -0700

    passing data to pug

--- controllers/CommunityNotesRouter.js
+++ controllers/CommunityNotesRouter.js
@@ -3,7 +3,7 @@ const CommunityNotesRouter = express.Router();
 
 
 CommunityNotesRouter.get('/notes', (req, res) => {
-    res.render('index');
+   return res.render('index', { data: "Hello world" });
 })
 
 module.exports = CommunityNotesRouter;
